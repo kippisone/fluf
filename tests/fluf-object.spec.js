19db7654d9dc32d928e1b356e144e93227b530a6
Andifeind <andifeind@noname-media.com>
2016-08-02 22:09:51 +0200

    Add obj.walk, utils.getType

--- tests/fluf-object.spec.js
+++ tests/fluf-object.spec.js
@@ -13,4 +13,45 @@ describe('fluf.Obj', function() {
       inspect(obj.get('foo')).isEql('bar');
     });
   });
+
+  describe('walk()', function() {
+    it('Walks recursive through an object', function() {
+      let obj = new Obj({
+        foo: 'bar',
+        bla: {
+          blub: 'blub 1',
+          blob: 'blob 2'
+        },
+        arr: ['one', 'two'],
+        arr2: [{ num: 'one'}, {num: 'two'}],
+        arr3: [['one', 'two'], ['three']]
+      });
+
+      let res = [];
+      obj.walk((value, key) => {
+        res.push(key + ':' + value);
+      });
+
+      inspect(res).isEql([
+        'foo:bar',
+        'bla:[object Object]',
+        'blub:blub 1',
+        'blob:blob 2',
+        'arr:one,two',
+        '0:one',
+        '1:two',
+        'arr2:[object Object],[object Object]',
+        '0:[object Object]',
+        'num:one',
+        '1:[object Object]',
+        'num:two',
+        'arr3:one,two,three',
+        '0:one,two',
+        '0:one',
+        '1:two',
+        '1:three',
+        '0:three'
+      ]);
+    });
+  });
 });
